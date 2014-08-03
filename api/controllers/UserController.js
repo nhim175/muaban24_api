a2eec7a3fe2af474daa4cc230871d4624ebcefac
Thinh Pham <nhim175@gmail.com>
2014-08-03 17:20:38 +0900

    User model, ctrl, login api

---
+++ api/controllers/UserController.js
@@ -0,0 +1,29 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+var md5 = require('MD5');
+var hat = require('hat');
+
+module.exports = {
+	login: function(req, res) {
+		var email = req.param('email');
+		var password = req.param('password');
+
+		User.findOne({ email: email }).exec(function (err, user) {
+      if (err) return res.send(err,500);
+      if (!user) return res.send("No other user with that email exists!", 404);
+      if (md5(password) !== user.password) {
+      	return res.send("Password is wrong!", 403);
+      }	else {
+     		user.token = hat();
+     		user.save(function() {
+     			res.json(user);
+     		});
+     	}
+    });
+	}
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	
+  attributes: {
+  	email: {
+			type: 'email',
+			required: true
+		},
+		password: {
+			type: 'STRING',
+			required: true
+		},
+		token: 'STRING'
+  }
+
+};
+
